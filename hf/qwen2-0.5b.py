2d5fd3eb6670f24cddfd04bef3db87191bc1ef59
catman626 <catman626@163.com>
2025-12-27 10:46:00 +0800

    checkpoint 12-27

--- hf/qwen2-0.5b.py
+++ hf/qwen2-0.5b.py
@@ -4,13 +4,12 @@ model_name_or_path = "Qwen/Qwen2-0.5B"
 
 generator = pipeline(
     "text-generation", 
-    model_name_or_path, 
-    torch_dtype="auto", 
-    device_map="auto",
-)
 
-messages = [
-    {"role": "user", "content": "Paris is the capital city of"},
-]
+)
+    # model_name_or_path, 
+    # ="auto", 
+# messages = [
+    # {"role": "user", "content": "Paris is the capital city of"},
+# ]
 messages = generator(messages, max_new_tokens=20)[0]["generated_text"]
 print(messages[-1]["content"])
